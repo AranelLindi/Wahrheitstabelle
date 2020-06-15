7cff7222c4bedfeaf861b70ea8967876516af25b
Stefan Lindörfer <stefan.lindoerfer@me.com>
2020-06-15 23:22:23 +0200

    Erste Version, zum Schreibschutz auf GitHub geladen

---
+++ main.c
@@ -0,0 +1,43 @@
+#include <stdio.h>                        // Konsolenausgabe
+
+/* TODO:
+*    - Logischen Ausdruck unter AUSDRUCK eingeben (Funktion anpassen)
+*    - ANZAHLBITS in Anzahl der Variablen des logischen Ausdrucks ändern
+*    - ZEILE 38 (Markierung!) entsprechend der Anzahl der Variablen anpassen
+*/
+
+#define AUSDRUCK(a, b, c, d)		!(a & b & c & d)
+#define ANZAHLBITS 4                     // entspricht Anzahl an Variablen
+
+// NICHTS ÄNDERN!
+#define SCHRANKE ((1 << ANZAHLBITS) - 1)    // Anzahl der Variationen (2^AnzahlBits)-1
+#define BYTE unsigned char                // zur Code-Vereinfachung
+
+// Globale Variable
+BYTE arr[ANZAHLBITS] = { 0 };                 // Array mit AnzahlBits-Feldern anlegen, enthält später gesetzte Bits
+
+int main(void) {
+    printf("****************************\n AUSGABE - WAHRHEITSTABELLE\n\n A B C D\tAUSDRUCK\n****************************\n");
+
+    for (int i = SCHRANKE; i >= 0; i--) {     // iteriert durch alle Zahlen von [Schranke; 0]
+	printf(" ");
+        // Variablen neuen Wert übergeben:
+        for(int j = 0; j < ANZAHLBITS; j++) { // iteriert durch Array
+            arr[j] = (i & 1 << j) ? 1 : 0;         // gibt 0 wenn j-tes Bit von i 'nicht gesetzt' und 1 wenn es 'gesetzt' ist zurück
+            printf("%i ", (arr[j]));     // Ausgabe
+        }
+        printf("\t");
+
+        int i = 0; // fungiert als Zurücksetzen in nächster Iteration
+
+        // **
+        // ZEILE ÄNDERN! Anzahl Variablen hier entsprechend
+        // anpassen.
+        if (AUSDRUCK(arr[0], arr[1], arr[2], arr[3])) i = 1; // bei true -> 1 setzen
+        // ***
+
+        printf("%5i\n", i); // Ausgabe und neue Zeile
+    }
+
+    return 0;
+}
